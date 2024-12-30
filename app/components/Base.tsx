8ed91145d5435d74219092acef09244963d34b05
Bailey Carroll <bailey@enchantedroseinnovations.com>
2024-12-30 12:41:57 -0800

    Reset for v2

--- app/components/Base.tsx
+++
@@ -1,7 +0,0 @@
-// Base Typography Component Props
-export default interface BaseProps {
-    children: React.ReactNode,
-    className?: string,
-    style?: React.CSSProperties,
-    id?: string,
-}
